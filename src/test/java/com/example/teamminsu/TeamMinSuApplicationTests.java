5236eae51cc2a1feee32525b3ef96bbd04641dbd
백민수 <257681691+minsu33@users.noreply.github.com>
2026-03-26 19:18:33 +0900

    Team02

---
+++ src/test/java/com/example/teamminsu/TeamMinSuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamminsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMinSuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
