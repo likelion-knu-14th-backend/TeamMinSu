cae524e96263b3c6ffe6f3af433a48092a6d07da
백민수 <257681691+minsu33@users.noreply.github.com>
2026-03-28 14:54:08 +0900

    첫 실습 과제

--- src/main/java/com/example/teamminsu/TeamMinSuApplication.java
+++ src/main/java/com/example/teamminsu/TeamMinSuApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamMinSuApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamMinSuApplication.class, args);
+
         System.out.println("");
     }
 
