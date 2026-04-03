4ada29a118c72d50d87825e854ff5fe820721dfa
백민수 <257681691+minsu33@users.noreply.github.com>
2026-04-03 12:07:03 +0900

    Revert "첫 실습 과제"
    This reverts commit 2de9db744a8560827831ee6be57c8ff30c734e36, reversing
    changes made to 936b73672c4ad9b28b51999ae4085cfd9af17214.

--- src/main/java/com/example/teamminsu/TeamMinSuApplication.java
+++ src/main/java/com/example/teamminsu/TeamMinSuApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamMinSuApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamMinSuApplication.class, args);
-
-        System.out.println("");
     }
 
 }
